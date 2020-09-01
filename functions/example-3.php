da9813f64564245133c6416d3253291999cf89e5
Nico Watine <nico@studioncreations.com>
2020-09-01 08:30:41 -0500

    New file for gutenberg editor

--- functions/example-3.php
+++ functions/example-3.php
@@ -22,15 +22,3 @@ remove_filter('pre_oembed_result', 'wp_filter_pre_oembed_result', 10);
 add_filter('embed_oembed_discover', '__return_false');
 
 add_action('wp_default_scripts', 'remove_jquery_migrate');
-
-
-// Fully Disable Gutenberg editor.
-add_filter('use_block_editor_for_post_type', '__return_false', 10);
-// Don't load Gutenberg-related stylesheets.
-add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
-function remove_block_css() {
-	wp_dequeue_style( 'wp-block-library' ); // WordPress core
-	wp_dequeue_style( 'wp-block-library-theme' ); // WordPress core
-	wp_dequeue_style( 'wc-block-style' ); // WooCommerce
-	wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront theme
-}
