9028bdbd92808bd262420ef0e5677bc2d01cef14
Nico Watine <nico@studioncreations.com>
2020-09-01 08:29:34 -0500

    New file

--- functions/example-3.php
+++ functions/example-3.php
@@ -21,20 +21,6 @@ remove_filter('pre_oembed_result', 'wp_filter_pre_oembed_result', 10);
 
 add_filter('embed_oembed_discover', '__return_false');
 
-
-//Remove JQuery migrate
-function remove_jquery_migrate($scripts)
-{
-	if (!is_admin() && isset($scripts->registered['jquery'])) {
-		$script = $scripts->registered['jquery'];
-
-		if ($script->deps) { // Check whether the script has any dependencies
-			$script->deps = array_diff($script->deps, array(
-				'jquery-migrate'
-			));
-		}
-	}
-}
 add_action('wp_default_scripts', 'remove_jquery_migrate');
 
 
---
+++ functions/jquery-migrate.php
@@ -0,0 +1,15 @@
+<?php
+
+//Remove JQuery migrate
+function remove_jquery_migrate($scripts)
+{
+	if (!is_admin() && isset($scripts->registered['jquery'])) {
+		$script = $scripts->registered['jquery'];
+
+		if ($script->deps) { // Check whether the script has any dependencies
+			$script->deps = array_diff($script->deps, array(
+				'jquery-migrate'
+			));
+		}
+	}
+}
