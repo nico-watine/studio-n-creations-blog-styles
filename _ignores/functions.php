acfd3dc01f354cfc75a07bcbbbf45067f55d692b
Nico Watine <nico@studioncreations.com>
2020-09-01 08:07:21 -0500

    More annotations

--- _ignores/functions.php
+++ _ignores/functions.php
@@ -6,13 +6,12 @@ remove_action('wp_head', 'wp_generator'); // Remove WordPress version number
 remove_action('wp_head', 'feed_links', 2);
 remove_action('wp_head', 'feed_links_extra', 3);
 remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'wlwmanifest_link'); // Remove wlwmanifest link
+remove_action('wp_head', 'wlwmanifest_link'); // Remove Windows Live Writer Manifest Link
 remove_action('wp_head', 'start_post_rel_link', 10, 0);
 remove_action('wp_head', 'parent_post_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0); // Remove shortlink
-remove_action('wp_head', 'rel_canonical');
 remove_action('wp_head', 'rel_alternate');
 remove_action('wp_head', 'wp_oembed_add_discovery_links');
 remove_action('wp_head', 'wp_oembed_add_host_js');
@@ -60,3 +59,6 @@ function remove_block_css() {
 	wp_dequeue_style( 'wc-block-style' ); // WooCommerce
 	wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront theme
 }
+
+// Optional removals
+// remove_action('wp_head', 'rel_canonical'); // Remove canonical tag
