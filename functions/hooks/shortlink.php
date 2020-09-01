0df766fd249cba5486ede28bb7b0bdeb7d6c3711
Nico Watine <nico@studioncreations.com>
2020-09-01 12:47:30 -0500

    Add additional singles

---
+++ functions/hooks/shortlink.php
@@ -0,0 +1,3 @@
+<?php
+
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0); // Remove shortlink
---
+++ functions/hooks/wlw-manifest.php
@@ -0,0 +1,3 @@
+<?php
+
+remove_action('wp_head', 'wlwmanifest_link'); // Remove Windows Live Writer Manifest Link
