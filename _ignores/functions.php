9f3222d1f052315c4c32e1657c7c02dc279111e1
Nico Watine <nico@studioncreations.com>
2020-09-01 07:37:15 -0500

    Add functions.php from personal avada build to ref

---
+++ _ignores/functions.php
@@ -0,0 +1,68 @@
+<?php
+
+function theme_enqueue_styles() {
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function avada_lang_setup() {
+	$lang = get_stylesheet_directory() . '/languages';
+	load_child_theme_textdomain( 'Avada', $lang );
+}
+add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+// Custom function Additions
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_head', 'rel_canonical');
+remove_action('wp_head', 'rel_alternate');
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
+remove_action('wp_head', 'rest_output_link_wp_head');
+remove_action( 'wp_head', 'wp_resource_hints', 2, 99 );
+
+remove_action('rest_api_init', 'wp_oembed_register_route');
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+remove_filter('pre_oembed_result', 'wp_filter_pre_oembed_result', 10);
+
+add_filter('embed_oembed_discover', '__return_false');
+
+
+//Remove JQuery migrate
+function remove_jquery_migrate($scripts)
+{
+	if (!is_admin() && isset($scripts->registered['jquery'])) {
+		$script = $scripts->registered['jquery'];
+
+		if ($script->deps) { // Check whether the script has any dependencies
+			$script->deps = array_diff($script->deps, array(
+				'jquery-migrate'
+			));
+		}
+	}
+}
+add_action('wp_default_scripts', 'remove_jquery_migrate');
+
+
+// Fully Disable Gutenberg editor.
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+// Don't load Gutenberg-related stylesheets.
+add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
+function remove_block_css() {
+	wp_dequeue_style( 'wp-block-library' ); // WordPress core
+	wp_dequeue_style( 'wp-block-library-theme' ); // WordPress core
+	wp_dequeue_style( 'wc-block-style' ); // WooCommerce
+	wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront theme
+}
