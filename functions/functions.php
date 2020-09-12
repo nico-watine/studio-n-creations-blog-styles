9ef88118c2958d14eee021fbd57c1b68f09932b7
Nico Watine <nico@studioncreations.com>
2020-09-11 22:46:53 -0500

    Add snippet

--- functions/functions.php
+++ functions/functions.php
@@ -13,3 +13,11 @@ remove_action('admin_print_scripts', 'print_emoji_detection_script' );
 remove_action('wp_print_styles', 'print_emoji_styles' );
 remove_action('admin_print_styles', 'print_emoji_styles' );
 // ----
+
+// Version CSS file in a theme
+wp_enqueue_style(
+	'theme-styles',
+	get_stylesheet_directory_uri() . '/style.css',
+	array(),
+	filemtime( get_stylesheet_directory() . '/style.css' )
+);
