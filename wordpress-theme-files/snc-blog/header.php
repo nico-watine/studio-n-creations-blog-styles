17c9c1d29b41f543f6d7ee00190a99c909421442
Nico Watine <nico@studioncreations.com>
2020-11-02 14:23:43 -0600

    New viewport meta tag

--- wordpress-theme-files/snc-blog/header.php
+++ wordpress-theme-files/snc-blog/header.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
